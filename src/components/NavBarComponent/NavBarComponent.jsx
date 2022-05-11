796aac1861baf220fde3d4dc10bba90c360cfff4
Carlos Hernandez Arce <krlosha30@gmail.com>
2022-05-10 19:46:19 -0600

    Improved inline documentation, implemented eventhandler for click and improved the template

--- src/components/NavBarComponent/NavBarComponent.jsx
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -1,21 +1,66 @@
-import React, { Component } from 'react';
-import './NavBarComponent.css';
-import home from '../../assets/home.svg'
+import React, { Component } from "react";
+import "./NavBarComponent.css";
+import home from "../../assets/home.svg";
 
-class NavBarComponent extends Component{
-constructor(){
+/**
+ * Navigation bar component
+ *
+ * @class NavBarComponent
+ * @extends {Component}
+ */
+class NavBarComponent extends Component {
+
+  /**
+   * Creates an instance of NavBarComponent.
+   * @memberof NavBarComponent
+   */
+  constructor() {
     super();
-}
- render() {
-        return (
-          <ul class="horizontal">
-            <li className='home'><img src={home}/></li>
-            <li><a href="#">Experiencia</a></li>
-            <li><a href="#">Estudios</a></li>
-            <li><a href="#">Habilidades</a></li>
-            <li><a href="#">Contactame</a></li>
-          </ul>
-        )
-    }
+    this.myRef = React.createRef();
+    this.options = ["Experiencia", "Estudios", "Habilidades", "Contacto"];
+  }
+
+  /**
+   * Method that cleans the active class of the li elements of the component
+   *
+   * @memberof NavBarComponent
+   */
+  cleanActive = () => {
+    [...this.myRef.current.children].forEach((li) => {
+      li.classList.remove("active");
+    });
+  };
+
+  /**
+   * Handler for the click event on the ul element of the component
+   *
+   * @param {*} e
+   * @memberof NavBarComponent
+   */
+  clickHandler = (e) => {
+    this.cleanActive();
+    e.target.parentElement.classList.add("active");
+  };
+
+  /**
+   * Method that renders the component
+   *
+   * @return {*} 
+   * @memberof NavBarComponent
+   */
+  render() {
+    return (
+      <ul className="horizontal" onClick={this.clickHandler} ref={this.myRef}>
+        <li key="Home"className="home active">
+          <img src={home} />
+        </li>
+        {this.options.map((o) => (
+          <li key={o}>
+            <a>{o}</a>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 export default NavBarComponent;
